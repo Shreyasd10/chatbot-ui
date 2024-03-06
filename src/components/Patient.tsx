48d1d9e5ccac0aeb8204bc28cbcacf4e72547c6f
Shreyasd10 <52241691+Shreyasd10@users.noreply.github.com>
2024-03-06 21:11:40 +0530

    back to render url

--- src/components/Patient.tsx
+++ src/components/Patient.tsx
@@ -30,7 +30,7 @@ const Patient = () => {
 
         // send form data to api endpoint
         await axios
-          .post("http://localhost:8000/post-audio", formData, {
+          .post("https://chatbot-backend-sbfd.onrender.com/post-audio", formData, {
             headers: {
               "Content-Type": "audio/mpeg",
             },
